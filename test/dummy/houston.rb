d01382eee489544db705a85e849de9fb68580f2a
Bob Lail <bob.lailfamily@gmail.com>
2018-10-02 10:10:01 -0500

    [skip] Corrected the Acceptance test (1m)

--- test/dummy/houston.rb
+++ test/dummy/houston.rb
@@ -10,6 +10,7 @@ Houston.config do
 
   # Give dummy values to these required fields.
   host "houston.test.com"
+  secret_key_base "2d1ab83477af2a340e48600c4550ed"
   mailer_sender "houston@test.com"
 
   # Mount this module on the dummy Houston application.
